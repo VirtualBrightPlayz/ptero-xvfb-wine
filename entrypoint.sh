77efc85629cb9193aed34a231abf0f35c99a7398
VirtualBrightPlayz <lego.trains.b@gmail.com>
2022-02-07 14:54:53 -0700

    yes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
